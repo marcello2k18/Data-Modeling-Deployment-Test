4ba5023895e32c6e00de4837b426cc81fad1cfe1
marcello2k18 <128180508+marcello2k18@users.noreply.github.com>
2024-05-11 08:37:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,34 @@
 import numpy as np
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import joblib
+import pickle
 
-# Create Flask application
+# Buat aplikasi Flask
 app = Flask(__name__)
 
-# Load the model using joblib
-model = joblib.load("model.pkl")
+# Muat model yang di-pickle
+model = pickle.load(open("model.pkl", "rb"))
+
 
 @app.route("/")
 def home():
     return render_template("index2.html")
 
+
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Get input features from the form and convert them to float
+    # Dapatkan fitur input dari formulir dan konversikan ke float
     float_features = [float(x) for x in request.form.values() if x]
     if len(float_features) != 5:
         return render_template("index2.html", prediction_result="Unknown")
 
     features = [np.array(float_features)]
 
-    # Perform prediction using the loaded model
+    # Lakukan prediksi menggunakan model yang dimuat
     prediction = model.predict(features)
 
-    # Pass the prediction result to HTML template
+    # Teruskan hasil prediksi ke template HTML
     return render_template("index2.html", prediction_result=prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
